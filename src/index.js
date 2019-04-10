1453314f57c223711e234a5b6cf63ca562090c70
Speros Kokenes <speroskokenes@gmail.com>
2019-04-10 11:04:28 -0400

    point to Axis server; updated enigma version

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 var enigma = require("enigma.js");
 var qixSchema = require("enigma.js/schemas/12.20.0.json");
 
+var appId = "43235e8d-415e-4931-9718-e5e3994dfe28";
+
 var config = {
-    schema: qixSchema,
-    url: "wss://playground.qlik.com/app/"
+  schema: qixSchema,
+  url: "wss://sense.axisgroup.com/app/" + appId,
+  createSocket: url => new WebSocket(url)
 };
 
 var session = enigma.create(config);
 
-session.open().then(function(global) {
-    return global.openDoc("952656cf-a3f5-42bc-bdad-9d2478031747");
-}).then(function(app) {
+session
+  .open()
+  .then(function(global) {
+    return global.openDoc(appId);
+  })
+  .then(function(app) {
     console.log("Got an App instance", app);
-});
+  });
