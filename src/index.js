09c95c54702f93d35e5729ec118e874d31a53bcb
Speros Kokenes <speroskokenes@gmail.com>
2019-04-10 11:07:23 -0400

    point to Axis server; bump enigma version

--- src/index.js
+++ src/index.js
@@ -9,52 +9,55 @@ var stateListDef = require("./defs/state-listobject.json");
 // Functions
 var renderFilter = require("./render-filter.js");
 
+var appId = "43235e8d-415e-4931-9718-e5e3994dfe28";
+
 var config = {
-    schema: qixSchema,
-    url: "wss://playground.qlik.com/app/"
+  schema: qixSchema,
+  url: "wss://sense.axisgroup.com/app/" + appId,
+  createSocket: url => new WebSocket(url)
 };
 
 var session = enigma.create(config);
 
 // A promise for an App instance
 var appPr = session.open().then(function(global) {
-    return global.openDoc("952656cf-a3f5-42bc-bdad-9d2478031747");
+  return global.openDoc(appId);
 });
 
 // Create the Region listbox with the App instance
-appPr.then(function(app) {
+appPr
+  .then(function(app) {
     return app.createSessionObject(regionListDef);
-})
-.then(function(regionLB) {
-
+  })
+  .then(function(regionLB) {
     var regionList = document.querySelector("#region");
 
     regionLB.getLayout().then(function(layout) {
-        renderFilter(regionList,layout, regionLB);
+      renderFilter(regionList, layout, regionLB);
     });
 
     regionLB.on("changed", function() {
-        regionLB.getLayout().then(function(layout) {
-            renderFilter(regionList, layout, regionLB);
-        });
+      regionLB.getLayout().then(function(layout) {
+        renderFilter(regionList, layout, regionLB);
+      });
     });
-});
+  });
 
 // Create the State listbox with the App instance
-appPr.then(function(app) {
+appPr
+  .then(function(app) {
     return app.createSessionObject(stateListDef);
-})
-.then(function(stateLB) {
-
+  })
+  .then(function(stateLB) {
     var stateList = document.querySelector("#state");
 
     stateLB.getLayout().then(function(layout) {
-        renderFilter(stateList,layout, stateLB);
+      renderFilter(stateList, layout, stateLB);
     });
 
     stateLB.on("changed", function() {
-        stateLB.getLayout().then(function(layout) {
-            renderFilter(stateList, layout, stateLB);
-        });
+      stateLB.getLayout().then(function(layout) {
+        renderFilter(stateList, layout, stateLB);
+      });
     });
-});
+  });
